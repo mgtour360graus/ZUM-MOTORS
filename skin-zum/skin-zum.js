995808ca65017a757af17dc59a6890ae11017b2d
MG TOUR 360 GRAUS <108066237+mgtour360graus@users.noreply.github.com>
2026-08-24 11:36:21 -0300

    COM,IIT9

--- skin-zum/skin-zum.js
+++ skin-zum/skin-zum.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const CONFIG = {
-    version: "1.0.4",
+    version: "1.0.5",
     assetsBase: "skin-zum/assets/",
     logo: "logo-zum.png",
     creditLogo: "logo-exata.png",
